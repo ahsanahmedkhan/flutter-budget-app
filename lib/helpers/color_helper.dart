dca6ba5078f39ab0d28ca60e8d63fe4ae75921b9
ahsanahmedkhan <Ahsanahmed.khan@hotmail.com>
2021-04-23 23:18:10 +0500

    Category Page

--- lib/helpers/color_helper.dart
+++ lib/helpers/color_helper.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 Color getColor(BuildContext context, double percent) {
-  if(percent >= 0.05){
+  if(percent >= 0.50){
     return Theme.of(context).primaryColor;
   }
   else if(percent >= 0.25){
---
+++ lib/screens/category_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_budget_app/models/category_model.dart';
+
+class CategoryScreen extends StatefulWidget {
+
+  final Category category;
+
+  CategoryScreen({this.category});
+
+  @override
+  _CategoryScreenState createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.category.name),
+        actions: <Widget> [
+          IconButton(icon: Icon(Icons.add),
+              iconSize: 30.0,
+              onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
