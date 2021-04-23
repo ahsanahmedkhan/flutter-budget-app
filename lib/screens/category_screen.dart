0dcac9c155d58da46d375a702472b197b68eec3d
ahsanahmedkhan <Ahsanahmed.khan@hotmail.com>
2021-04-23 23:26:52 +0500

    Custom Painter

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_budget_app/models/category_model.dart';
 
@@ -22,6 +23,29 @@ class _CategoryScreenState extends State<CategoryScreen> {
               onPressed: () {},
           )
         ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget> [
+            Container(
+              margin: EdgeInsets.all(20.0),
+              padding: EdgeInsets.all(20.0),
+              height: 250.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black12,
+                    offset: Offset(0, 2),
+                    blurRadius: 6.0,
+                  )
+                ]
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
